c6b8e937d08ce9ffd323ab2b1d05676ff75b7ef2
lucassvalentim <lucasvalentimrocha@gmail.com>
2024-11-10 22:35:33 -0300

    metodosImplementados

--- metodoNewton.c
+++ metodoNewton.c
@@ -6,15 +6,10 @@ double f(double x){
     return exp(-(x*x)) - cos(x);
 }
 
-double phi(double x){
-    return cos(x) - exp(-(x*x)) + x;
-    // return ((x*x*x) + 3) / 9;
+double derivada_f(double x){
+    return (-2)*exp(-(x*x))*x + sin(x);
 }
 
-double derivada_phi(double x){
-    return -sin(x) + 2*x*exp(-(x*x)) + 1;
-    // return (x*x*x) / 3;
-}
 
 int main(){
 
@@ -25,33 +20,24 @@ int main(){
     //precisao
     double erro = 1e-4;
 
-    //verificar de a derivada de phi é menor que um dentre todo o intervalo
-    for(double i = a; i < b; i+=0.15){
-        if(derivada_phi(i) >= 1){
-            printf("Nao eh possivel realizar o metodo, pois: derivada_phi(%lf) > 1\n", i);
-        }
-    }
-
     double x_anterior = 1.5;
-    double x_atual = phi(x_anterior);
+    double x_atual = x_anterior - (f(x_anterior)/derivada_f(x_anterior));
     
     int k = 0;
     printf("==================== iteracao %d ====================\n", k);
     printf("X%d = %lf\n", k, x_anterior);
-    printf("phi(%lf) = %lf\n", x_anterior, x_atual);
     printf("f(%lf) = %lf\n", x_atual, f(x_atual));
     printf("\n");
     
     while((f(x_atual) < 0 ? f(x_atual) * (-1.0) : f(x_atual)) > erro){
         
         x_anterior = x_atual;
-        x_atual = phi(x_anterior);
+        x_atual = x_anterior - (f(x_anterior)/derivada_f(x_anterior));
         k++;
         
         printf("==================== iteracao %d ====================\n", k);
 
         printf("X%d = %lf\n", k, x_anterior);
-        printf("phi(%lf) = %lf\n", x_anterior, x_atual);
         printf("f(%lf) = %.9lf\n", x_atual, f(x_atual));
         printf("\n");
     }
---
+++ metodoSecante.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double f(double x){
+    return exp(-(x*x)) - cos(x);
+}
+
+int main(){
+
+    //intervalo
+    int a = 1;
+    int b = 2;
+
+    //precisao
+    double erro = 1e-4;
+
+    double x_anterior1 = 1;
+    double x_anterior2 = 2;
+
+    double derivada_aproximada = (f(x_anterior2) - f(x_anterior1)) / (x_anterior2 - x_anterior1);
+    double x_atual = x_anterior2 - f(x_anterior2) / derivada_aproximada;
+    
+    int k = 0;
+    printf("==================== iteracao %d ====================\n", k);
+    printf("X%d = %lf\n", k, x_anterior1);
+    printf("X%d = %lf\n", k+1, x_anterior2);
+    printf("f(%lf) = %lf\n", x_atual, f(x_atual));
+    printf("\n");
+    
+    while((f(x_atual) < 0 ? f(x_atual) * (-1.0) : f(x_atual)) > erro){
+        
+        x_anterior1 = x_anterior2;
+        x_anterior2 = x_atual;
+
+        derivada_aproximada = (f(x_anterior2) - f(x_anterior1)) / (x_anterior2 - x_anterior1);
+        x_atual = x_anterior2 - f(x_anterior2) / derivada_aproximada;
+        
+        k++;
+        
+        printf("==================== iteracao %d ====================\n", k);
+
+        printf("X%d = %lf\n", k, x_anterior1);
+        printf("X%d = %lf\n", k+1, x_anterior2);
+
+        printf("f(%lf) = %.9lf\n", x_atual, f(x_atual));
+        printf("\n");
+    }
+
+    printf("X%d = %lf\nNumero de iteracoes: %d\n", k+1, x_atual, k+1);
+    printf("f(x) = %.9lf\n", f(x_atual));
+    return 0;
+}
